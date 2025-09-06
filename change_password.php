ff709859942d273900fcc9f24d2698be89bc9f39
AlissonWenceslau <alisson.wenceslau@gmail.com>
2025-09-06 12:54:39 -0300

    Fix: Icones das mensagens de sucesso e erro

--- change_password.php
+++ change_password.php
@@ -114,10 +114,12 @@ $conn->close();
       if (isset($_SESSION['password_updated'])) {
         echo "<div class='alert alert-success' id='alert' role='alert'>";
         echo "
-              <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-check-fill' viewBox='0 0 16 16'>
-              <path fill-rule='evenodd' d='M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0'/>
-              <path d='M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6'/>
+              <svg xmlns='http://www.w3.org/2000/svg' style='display: none;' viewBox='0 0 16 16'>
+                <symbol id='check-circle-fill' fill='currentColor' viewBox='0 0 16 16'>
+                  <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>
+                </symbol>
               </svg>
+               <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'><use xlink:href='#check-circle-fill'/></svg>
               ";
         echo $_SESSION['password_updated'];
         echo "</div>";
@@ -128,12 +130,14 @@ $conn->close();
         foreach ($_SESSION['errors'] as $erro) {
           echo "<div class='alert alert-danger updated-password-fail alert-dismissible fade show' id='alert' role='alert'>";
           echo "
-                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-fill-x' viewBox='0 0 16 16'>
-                  <path d='M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4'/>
-                  <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708'/>
+                <svg xmlns='http://www.w3.org/2000/svg' style='display: none;'>
+                  <symbol id='exclamation-triangle-fill' fill='currentColor' viewBox='0 0 16 16'>
+                    <path d='M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z'/>
+                  </symbol>
                 </svg>
-                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-                ";
+              <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
+              <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              ";
           echo $erro;
           echo "</div>";
         }

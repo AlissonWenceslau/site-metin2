12eacb9463593735b6ac33038f7bc42ff696603f
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2026-06-05 00:07:05 -0300

    Organizar assets

--- pages/utils/utils.php
+++ pages/utils/utils.php
@@ -3,19 +3,19 @@ function pgClass($job)
 {
     switch ($job) {
         case 0:
-            return '<img src="../assets/ranking/0.png">';
+            return '<img src="../assets/character/0.png">';
         case 1:
-            return '<img src="../assets/ranking/1.png">';
+            return '<img src="../assets/character/1.png">';
         case 2:
-            return '<img src="../assets/ranking/2.png">';
+            return '<img src="../assets/character/2.png">';
         case 3:
-            return '<img src="../assets/ranking/3.png">';
+            return '<img src="../assets/character/3.png">';
         case 4:
-            return '<img src="../assets/ranking/4.png">';
+            return '<img src="../assets/character/4.png">';
         case 5:
-            return '<img src="../assets/ranking/5.png">';
+            return '<img src="../assets/character/5.png">';
         case 6:
-            return '<img src="../assets/ranking/6.png">';
+            return '<img src="../assets/character/6.png">';
         case 7:
             return '<img src="../assets/ranking/7.png">';
     }
@@ -25,11 +25,11 @@ function pgKingdom($kingdom)
 {
     switch ($kingdom) {
         case 1:
-            return '<img src="../assets/ranking/shinso.jpg">';
+            return '<img src="../assets/kingdom/shinso.jpg">';
         case 2:
-            return '<img src="../assets/ranking/chunjo.jpg">';
+            return '<img src="../assets/kingdom/chunjo.jpg">';
         case 3:
-            return '<img src="../assets/ranking/jinno.jpg">';
+            return '<img src="../assets/kingdom/jinno.jpg">';
     }
 }
 
